b79de347c1504d0a543c1efcef3c3af9ba651ffa
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-07 12:52:39 -0600

    made a better verion of my previous stupid projects

---
+++ src/supercool/controller/ProccessController.java
@@ -0,0 +1,10 @@
+package supercool.controller;
+
+public class ProccessController
+{
+	public void start()
+	{
+		System.out.println("I'm a super cool project!");
+		System.out.println("Wha u got to say about that?");
+	}
+}
---
+++ src/supercool/controller/ProcessRunner.java
@@ -0,0 +1,10 @@
+package supercool.controller;
+
+public class ProcessRunner
+{
+	public static void main (String [] args)
+	{
+		ProccessController appController = new ProccessController();
+		appController.start();
+	}
+}
