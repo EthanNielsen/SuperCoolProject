d10e83a3450d8da0aefd23080dc7c2eb69035147
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-11 12:14:21 -0600

    I made a play doh circcle and printed it.

--- src/supercool/controller/ProcessController.java
+++ src/supercool/controller/ProcessController.java
@@ -1,10 +1,24 @@
 package supercool.controller;
 
+import supercool.model.PlayDohCircle;
+
 public class ProcessController
 {
+	public ProcessController()
+	{
+		
+	}
+	
 	public void start()
 	{
 		System.out.println("I'm a super cool project!");
 		System.out.println("Wha u got to say about that?");
+		
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle();
+		
+		System.out.println("LooK I made a circle");
+		System.out.println(firstCircle);
 	}
 }
