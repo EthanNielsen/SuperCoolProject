e3f39972e7753bc6d054d2f2457adad916f3e4f3
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-25 12:06:16 -0600

    I am putting something into my data members for "words" list

--- src/supercool/controller/ProcessController.java
+++ src/supercool/controller/ProcessController.java
@@ -10,53 +10,61 @@ public class ProcessController
 	
 	
 	private int[] numbers;
-	private String[] names;
+	private String[] words;
 	
 	
 	public ProcessController()
 	{
-		
+		numbers = new int[10];
+		words = new String[10];
 	}
 	
 	public void start()
 	{
+		System.out.println(words);
+		System.out.println(numbers);
 		
-		numbers = new int[10];
-		names = new String[10];
-		
-		
-		System.out.println("I'm a super cool project!");
-		System.out.println("Wha u got to say about that?");
-		
-		PlayDohCircle firstCircle = new PlayDohCircle();
-		PlayDohCircle secondCircle;
-		secondCircle = new PlayDohCircle();
-		
-		System.out.println("LooK I made a circle");
-		System.out.println(firstCircle);
-		System.out.println(secondCircle);
-		
-		PlayDohCircle thirdCircle = new PlayDohCircle(10);
-		System.out.println(thirdCircle);
+		System.out.println(words[0]);
+		System.out.println(words[1]);
+		System.out.println(words[2]);
 		
 		
-		CookieClicker firstClick = new CookieClicker();
-		CookieClicker secondClick;
-		secondClick = new CookieClicker();
-		
-		Scanner myScanner = new Scanner(System.in);
-		System.out.println("How many click do you want?");
-		int circleSize = myScanner.nextInt();
 		
 		
-		System.out.println("Oh man.. I can hook you up with some good clicks..");
-		System.out.println(firstClick);
 		
-		System.out.println("How many clicks should we change it to?");
-		int updated = myScanner.nextInt();
-		thirdCircle.setSize(updated);
-		System.out.println(thirdCircle);
 		
+//		System.out.println("I'm a super cool project!");
+//		System.out.println("Wha u got to say about that?");
+//		
+//		PlayDohCircle firstCircle = new PlayDohCircle();
+//		PlayDohCircle secondCircle;
+//		secondCircle = new PlayDohCircle();
+//		
+//		System.out.println("LooK I made a circle");
+//		System.out.println(firstCircle);
+//		System.out.println(secondCircle);
+//		
+//		PlayDohCircle thirdCircle = new PlayDohCircle(10);
+//		System.out.println(thirdCircle);
+//		
+//		
+//		CookieClicker firstClick = new CookieClicker();
+//		CookieClicker secondClick;
+//		secondClick = new CookieClicker();
+//		
+//		Scanner myScanner = new Scanner(System.in);
+//		System.out.println("How many click do you want?");
+//		int circleSize = myScanner.nextInt();
+//		
+//		
+//		System.out.println("Oh man.. I can hook you up with some good clicks..");
+//		System.out.println(firstClick);
+//		
+//		System.out.println("How many clicks should we change it to?");
+//		int updated = myScanner.nextInt();
+//		thirdCircle.setSize(updated);
+//		System.out.println(thirdCircle);
+//		
 	//	Scanner sc = new Scanner(System.in);
 	//	int i = sc.nextInt (yes);
 		
